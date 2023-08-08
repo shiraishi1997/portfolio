6892ae6fbaf11d981049d210b2dcfc02643f253e
shiraishi1997 <shiraishi.0904.aaa@gmail.com>
2023-08-08 13:46:19 +0900

    migtation修正

--- database/migrations/2023_07_26_151917_create_order_product_table.php
+++ database/migrations/2023_07_26_151917_create_order_product_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('order_product', function (Blueprint $table) {
             
             $table->foreignId('order_id')->constrained('orders');
-            $table->foreignId('product_id')->constrained('product');
+            $table->foreignId('product_id')->constrained('products');
         });
     }
 
