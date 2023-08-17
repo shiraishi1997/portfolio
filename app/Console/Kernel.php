7d888eb1ceed177fbf2de1537cbe5d5e9b03532b
shiraishi1997 <133613587+shiraishi1997@users.noreply.github.com>
2023-08-17 21:07:27 +0900

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(OrderDuplicate::class)->daily();
-        $schedule->command(Redupulicate::class)->daily();
+        $schedule->command('OrderDuplicate')->daily();
+        $schedule->command('Redupulicate')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
